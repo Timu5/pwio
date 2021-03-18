5af3c78d927fdfe9e03b43c6ab2c8e02e95f6244
Agnieszka <baga16@o2.pl>
2021-03-18 21:50:56 +0100

    Change order of drawing

--- utils.py
+++ utils.py
@@ -1,25 +1,43 @@
 from numba import jit
 
+
 @jit(nopython=True, nogil=True)
 def blit(dest, src, loc):
     sy, sx, _ = src.shape
     dx = sx + loc[1]
     dy = sy + loc[0]
     if dx >= dest.shape[1]:
-        sx -= (dx - dest.shape[1])
+        sx -= dx - dest.shape[1]
     if dy >= dest.shape[0]:
-        sy -= (dy - dest.shape[0])
+        sy -= dy - dest.shape[0]
 
-    dest[loc[0]:loc[0]+sy, loc[1]:loc[1]+sx, :] = src[:sy, :sx, :]
+    dest[loc[0] : loc[0] + sy, loc[1] : loc[1] + sx, :] = src[:sy, :sx, :]
 
 
-def order2D(width, height):
+def order2D(m, n):
+    k = 0  # row
+    l = 0  # column
     elements = []
-    for y in range(height):
-        for x in range(width):
-            a = (y-height/2 + 0.5)
-            b = (x-width/2 + 0.5)
-            distance = max(abs(a), abs(b))
-            elements.append((distance, x, y))
-    elements.sort(key=lambda x: x[0])
-    return elements
+
+    while k < m and l < n:
+
+        for i in range(l, n):
+            elements.append((0, k, i))
+        k += 1
+
+        for i in range(k, m):
+            elements.append((0, i, n - 1))
+        n -= 1
+
+        if k < m:
+            for i in range(n - 1, (l - 1), -1):
+                elements.append((0, m - 1, i))
+            m -= 1
+
+        if l < n:
+            for i in range(m - 1, k - 1, -1):
+                elements.append((0, i, l))
+            l += 1
+
+    el = elements[::-1]
+    return el
